e126002b8e57f52899fda2704134d5875a224300
philemonmurangiri <murangirilesx890@gmail.com>
2024-03-31 20:37:50 +0300

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const farmAnimals = 'cow horse sheep pig chicken';
 
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+
+const [bessie, , dolly, babe, little] = farmAnimals.split(' ');
+
+const [blackAndWhite, , black, pink] = farmAnimals.split(' ');
+
+
+
+
+
+
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+const [r, o, y, g, b, , v] = colors; 
+const [, , , , , indg] = colors; 
+
+
+
 
 const muppet = {
   muppetName: 'Miss Piggy',
@@ -24,6 +41,28 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+
+const { 
+  muppetName, 
+  color, 
+  song, 
+  job, 
+  partner 
+} = muppet;
+
+const { 
+  nestedName, 
+  nestedColor, 
+  nestedJob, 
+  nestedPartner, 
+  album: { 
+    theMuppetMovie: { 
+      song2, 
+      song4 
+    } 
+  } 
+} = nestedMuppet;
+
 
 // Strings
 
